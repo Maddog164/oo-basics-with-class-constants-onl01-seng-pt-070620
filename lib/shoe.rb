be3b732dd4392cab077e75cc699fc98f781ab8de
Denise Henderson <denise@henderson3.us>
2020-07-23 00:23:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,25 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    i = 0
+    binding.pry
+    while BRANDS.length < i do
+      if brand == BRANDS[i]
+        trigger = "dontadd"
+        i = BRANDS.length
+      end
+      i += 1
+    end
+    if trigger != "dontadd"
+      BRANDS << brand
+    end
   end
 
   def cobble
